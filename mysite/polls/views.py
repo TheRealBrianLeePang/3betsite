96fd13bb23be3b406ae75a2099e4f0fe9469004e
david19kerrigan <david19kerrigan@gmail.com>
2020-02-27 20:24:01 -0500

    done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,18 +13,16 @@ def addZero(num):
     else:
         return str(num)
 
-print('Beginning file download with urllib2...')
 
 
 today = date.today()
 year = str(today.year)
 month = addZero(today.month)
-day = addZero(today.day+1)
+day = addZero(today.day)
 scheduleYear = ''
 games = []
 
 url = 'http://data.nba.net/10s/prod/v1/'+year+month+day+'/scoreboard.json'
-print(url)
 urllib.request.urlretrieve(url, 'test.json')
 url = 'http://data.nba.net/10s/prod/v1/today.json'
 urllib.request.urlretrieve(url, 'frontpage.json')
@@ -32,14 +30,10 @@ urllib.request.urlretrieve(url, 'frontpage.json')
 with open('test.json') as json_file:
     data = json.load(json_file)
     for p in data['games']:
-        print('vTeam: ' + str(p['vTeam']['teamId']))
-        print('hTeam: ' + str(p['hTeam']['teamId']))
         games.append([p['vTeam']['teamId'],p['hTeam']['teamId']])
-        print('')
 with open('frontpage.json') as json_file:
     data = json.load(json_file)
     scheduleYear = data['seasonScheduleYear']
-print(scheduleYear)
 url = 'http://data.nba.net/10s//prod/'+str(scheduleYear)+'/teams_config.json'
 urllib.request.urlretrieve(url, 'teamconfig.json')
 teamNames = deepcopy(games)
@@ -47,13 +41,25 @@ for index,i in enumerate(games):
     for jindex,j in enumerate(i):
         name = teams.find_team_name_by_id(j)
         teamNames[index][jindex] = name['full_name']
-print(games[0][0])
-teamDict = commonteamroster.CommonTeamRoster(games[0][0]).get_dict()
-for key in teamDict["resultSets"]:
-    print(key)
-print(teamDict["resultSets"])
+def getPlayers(teamId):
+    teamDict = commonteamroster.CommonTeamRoster(teamId).get_dict()
+    players = ""
+    for i in teamDict["resultSets"][0]["rowSet"]:
+        players+=(i[3])+", "
+    return players
+
+result = ""
+for index,i in enumerate(teamNames):
+    for jindex,j in enumerate(i):
+        result+=str(j)
+        if(jindex == 0):
+            result+= " -- "+getPlayers(games[index][jindex]) + "<br>"
+            result+="VS <br>"
+        else:
+            result+= " -- "+getPlayers(games[index][jindex]) + "<br>"
+    result+="<br>"
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(str(teamNames)+" yo here be the games")
+    return HttpResponse(result)
 
