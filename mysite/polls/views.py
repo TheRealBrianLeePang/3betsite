44323b8b0fe1cea2fe3a9e24868ef439047056b3
david19kerrigan <david19kerrigan@gmail.com>
2020-04-30 18:46:50 -0400

    fix

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,148 +1,147 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import urllib.request
-from datetime import date
+from datetime import date, timedelta
 import json
 from copy import copy, deepcopy
 import os.path
 from os import path
 import ast
 
-def addZero(num):
-    if len(str(num)) == 1:
-        return '0'+str(num)
-    else:
-        return str(num)
-
-
-
-today = date.today()
-year = str(today.year)
-month = addZero(int(today.month)-3)
-tempDay = today.day
-day = addZero(tempDay)
-scheduleYear = ''
-games = []
-arenas = []
-
-
-url = 'http://data.nba.net/10s/prod/v1/'+year+month+day+'/scoreboard.json'
-print(url)
-#urllib.request.urlretrieve(url, 'scoreboard.json')
-url = 'http://data.nba.net/10s/prod/v1/today.json'
-#urllib.request.urlretrieve(url, 'frontpage.json')
-
-with open('data/'+year+month+day+'.json') as json_file:
-    data = json.load(json_file)
-    for p in data['games']:
-        games.append([p['vTeam']['teamId'],p['hTeam']['teamId']])
-        arenas.append(p['arena']['name']+ ", " + p['arena']['city'])
-
-with open('frontpage.json') as json_file:
-    data = json.load(json_file)
-    scheduleYear = data['seasonScheduleYear']
-url = 'http://data.nba.net/10s//prod/'+str(scheduleYear)+'/teams_config.json'
-#urllib.request.urlretrieve(url, 'teamconfig.json')
-
-with open('polls/teamDictionary.json') as json_file:
-    teamDict = json.load(json_file)
-
-teamNames = deepcopy(games)
-for index,i in enumerate(games):
-    for jindex,j in enumerate(i):
-        name = teamDict[j]
-        teamNames[index][jindex] = name
-savedDict = {}
-def getPlayers(teamId):
-    if path.exists("players"+year+month+day+".txt"):
-        
-        with open("players"+year+month+day+".txt", 'r') as f:
-            s = f.read()
-            tempDict = ast.literal_eval(s)
-        return tempDict[teamId]
-    else:
-
-        players = ""
-        url = 'http://data.nba.net/10s/prod/v1/'+str(int(year)-1)+'/teams/'+teamId+'/roster.json'
-        urllib.request.urlretrieve(url, 'data/'+teamId+'.json')
-        with open(teamId+'.json') as json_file:
-            data = json.load(json_file)
-            for p in data['league']['standard']['players']:
-                players += p['personId']+","
-        savedDict[teamId] = players
-        if len(savedDict) == len(teamNames) * 2:
-            print(teamNames)
-            with open("players"+year+month+day+".txt", 'w') as f:
-                print(savedDict, file=f)
-        return players
-        '''
-        teamDict = commonteamroster.CommonTeamRoster(teamId).get_dict()
-        players = ""
-        for i in teamDict["resultSets"][0]["rowSet"]:
-            players+=(i[3])+", "
-        savedDict[teamId] = players
-        if len(savedDict) == len(teamNames) * 2:
-            print(teamNames)
-            with open("players"+year+month+day+".txt", 'w') as f:
-                print(savedDict, file=f)
-        return players
-        '''
-
-def getPlayerName(playerId):
-    url = "http://data.nba.net/10s/prod/v1/"+str(int(year)-1)+"/players.json"
-    urllib.request.urlretrieve(url, 'players.json')
-    with open('players.json') as json_file:
-        data = json.load(json_file)
-        for p in data['league']['standard']:
-            if p['personId'] == playerId:
-                return p['firstName']+" "+p['lastName']
-
-
-result = "<!DOCTYPE html>"
-result += "<html>"
-result += "<head>"
-result += "<meta charset='utf-8'>"
-result += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-result += "<title>NBA Games</title>"
-result += "<link rel='stylesheet' href='https://unpkg.com/purecss@1.0.1/build/pure-min.css'>"
-result += "<style>img{display: block; margin-left: auto; margin-right: auto;}"
-result += ".tooltip {position: relative; display: inline-block; border-bottom: 1px dotted black;}"
-result += ".tooltip .tooltiptext {visibility: hidden; width: 200px; background-color: black; color: white; text-align: center; border-radius: 6px; padding: 5px 0; top: 100%; left: 50%; margin-left: -100px; position: absolute; z-index: 1;}"
-result += ".tooltip:hover .tooltiptext {visibility: visible;}"
-result += ".center {margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}"
-result += "body{color: white; font-weight: bold; background-image: url('https://cdn.nba.net/assets/video/logos/nba-placeholder.jpg'); background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;}</style>"
-result += "</head><div class='center'><body><h1 align='center'>NBA Games Happening on " + month + "/" + day + "/" + year + ":</h1>"
-result += "<h3 style = 'text-align:center'><i>(If COVID-19 didn't exist!)</i></h3>"
-result += "<table align='center' class='pure-table pure-table-bordered'><thead><tr><th>Away Team</th><th>Home Team</th><th>Location</th><th>Winner/Spread</th></thead>"
-
-with open('polls/playerDictionary.json') as json_file:
-    playerDict = json.load(json_file)
-
-for index,i in enumerate(teamNames):
-    result += "<tr>"
-    for jindex,j in enumerate(i):
-        
-        playernames = playerDict[games[index][jindex]]
-        playernames = playernames.split(",")
-        
-        result += "<td>"
-        
-        if jindex % 2 == 0:
-            result += "<div class='tooltip'>" + str(j) + "<span class='tooltiptext'>" 
-            for p in playernames[:-1]:
-                result += str(p) + "<br>"
-            result += playernames[-1] + "</span></div></td>"
-        else:
-            result += "<div class='tooltip'>" + str(j) + "<span class='tooltiptext'>" 
-            for p in playernames[:-1]:
-                result += str(p) + "<br>"
-            result += playernames[-1] + "</span></div></td>"
-            result += "<td>" + arenas[index]  + "<td></td> "
-
-    result += "</tr>"
-result+= "</table><br><a href = 'https://stats.nba.com/scores/03/11/2020'><img src='http://loodibee.com/wp-content/uploads/nba-logo-transparent.png' alt='NBA logo' height='150'></a>"
-result+= "<p style = 'text-align:center; font-size:12px'>Project 3bet, Case Western Reserve Univeristy 2020 Senior Project <br> David Greenberg, Brian Pang, Lucas Invernizzi, Kevin Szmyd, David Kerrigan</p></body></html>"
 
 
 def index(request):
+    today = date.today() - timedelta(days=90)
+    def addZero(num):
+        if len(str(num)) == 1:
+            return '0'+str(num)
+        else:
+            return str(num)
+
+    year = str(today.year)
+    day = addZero(today.day)
+    month = addZero(today.month)
+    scheduleYear = ''
+    games = []
+    arenas = []
+
+
+    url = 'http://data.nba.net/10s/prod/v1/'+year+month+day+'/scoreboard.json'
+    print(url)
+    #urllib.request.urlretrieve(url, 'scoreboard.json')
+    url = 'http://data.nba.net/10s/prod/v1/today.json'
+    #urllib.request.urlretrieve(url, 'frontpage.json')
+
+    with open('data/'+year+month+day+'.json') as json_file:
+        data = json.load(json_file)
+        for p in data['games']:
+            games.append([p['vTeam']['teamId'],p['hTeam']['teamId']])
+            arenas.append(p['arena']['name']+ ", " + p['arena']['city'])
+
+    with open('frontpage.json') as json_file:
+        data = json.load(json_file)
+        scheduleYear = data['seasonScheduleYear']
+    url = 'http://data.nba.net/10s//prod/'+str(scheduleYear)+'/teams_config.json'
+    #urllib.request.urlretrieve(url, 'teamconfig.json')
+
+    with open('polls/teamDictionary.json') as json_file:
+        teamDict = json.load(json_file)
+
+    teamNames = deepcopy(games)
+    for index,i in enumerate(games):
+        for jindex,j in enumerate(i):
+            name = teamDict[j]
+            teamNames[index][jindex] = name
+    savedDict = {}
+    def getPlayers(teamId):
+        if path.exists("players"+year+month+day+".txt"):
+            
+            with open("players"+year+month+day+".txt", 'r') as f:
+                s = f.read()
+                tempDict = ast.literal_eval(s)
+            return tempDict[teamId]
+        else:
+
+            players = ""
+            url = 'http://data.nba.net/10s/prod/v1/'+str(int(year)-1)+'/teams/'+teamId+'/roster.json'
+            urllib.request.urlretrieve(url, 'data/'+teamId+'.json')
+            with open(teamId+'.json') as json_file:
+                data = json.load(json_file)
+                for p in data['league']['standard']['players']:
+                    players += p['personId']+","
+            savedDict[teamId] = players
+            if len(savedDict) == len(teamNames) * 2:
+                print(teamNames)
+                with open("players"+year+month+day+".txt", 'w') as f:
+                    print(savedDict, file=f)
+            return players
+            '''
+            teamDict = commonteamroster.CommonTeamRoster(teamId).get_dict()
+            players = ""
+            for i in teamDict["resultSets"][0]["rowSet"]:
+                players+=(i[3])+", "
+            savedDict[teamId] = players
+            if len(savedDict) == len(teamNames) * 2:
+                print(teamNames)
+                with open("players"+year+month+day+".txt", 'w') as f:
+                    print(savedDict, file=f)
+            return players
+            '''
+
+    def getPlayerName(playerId):
+        url = "http://data.nba.net/10s/prod/v1/"+str(int(year)-1)+"/players.json"
+        urllib.request.urlretrieve(url, 'players.json')
+        with open('players.json') as json_file:
+            data = json.load(json_file)
+            for p in data['league']['standard']:
+                if p['personId'] == playerId:
+                    return p['firstName']+" "+p['lastName']
+
+
+    result = "<!DOCTYPE html>"
+    result += "<html>"
+    result += "<head>"
+    result += "<meta charset='utf-8'>"
+    result += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+    result += "<title>NBA Games</title>"
+    result += "<link rel='stylesheet' href='https://unpkg.com/purecss@1.0.1/build/pure-min.css'>"
+    result += "<style>img{display: block; margin-left: auto; margin-right: auto;}"
+    result += ".tooltip {position: relative; display: inline-block; border-bottom: 1px dotted black;}"
+    result += ".tooltip .tooltiptext {visibility: hidden; width: 200px; background-color: black; color: white; text-align: center; border-radius: 6px; padding: 5px 0; top: 100%; left: 50%; margin-left: -100px; position: absolute; z-index: 1;}"
+    result += ".tooltip:hover .tooltiptext {visibility: visible;}"
+    result += ".center {margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}"
+    result += "body{color: white; font-weight: bold; background-image: url('https://cdn.nba.net/assets/video/logos/nba-placeholder.jpg'); background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;}</style>"
+    result += "</head><div class='center'><body><h1 align='center'>NBA Games Happening on " + month + "/" + day + "/" + year + ":</h1>"
+    result += "<h3 style = 'text-align:center'><i>(If COVID-19 didn't exist!)</i></h3>"
+    result += "<table align='center' class='pure-table pure-table-bordered'><thead><tr><th>Away Team</th><th>Home Team</th><th>Location</th><th>Winner/Spread</th></thead>"
+
+    with open('polls/playerDictionary.json') as json_file:
+        playerDict = json.load(json_file)
+
+    for index,i in enumerate(teamNames):
+        result += "<tr>"
+        for jindex,j in enumerate(i):
+            
+            playernames = playerDict[games[index][jindex]]
+            playernames = playernames.split(",")
+            
+            result += "<td>"
+            
+            if jindex % 2 == 0:
+                result += "<div class='tooltip'>" + str(j) + "<span class='tooltiptext'>" 
+                for p in playernames[:-1]:
+                    result += str(p) + "<br>"
+                result += playernames[-1] + "</span></div></td>"
+            else:
+                result += "<div class='tooltip'>" + str(j) + "<span class='tooltiptext'>" 
+                for p in playernames[:-1]:
+                    result += str(p) + "<br>"
+                result += playernames[-1] + "</span></div></td>"
+                result += "<td>" + arenas[index]  + "<td></td> "
+
+        result += "</tr>"
+    result+= "</table><br><a href = 'https://stats.nba.com/scores/03/11/2020'><img src='http://loodibee.com/wp-content/uploads/nba-logo-transparent.png' alt='NBA logo' height='150'></a>"
+    result+= "<p style = 'text-align:center; font-size:12px'>Project 3bet, Case Western Reserve Univeristy 2020 Senior Project <br> David Greenberg, Brian Pang, Lucas Invernizzi, Kevin Szmyd, David Kerrigan</p></body></html>"
+
+
     return HttpResponse(result)
